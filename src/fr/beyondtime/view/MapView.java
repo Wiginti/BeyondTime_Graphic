8f638b77f5098fec650d45ec13fbbf1350bd675a
Ruubinho <166310534+Ruubinho@users.noreply.github.com>
2025-02-21 11:54:03 +0100

    Create MapView

---
+++ src/fr/beyondtime/view/MapView.java
@@ -0,0 +1,39 @@
+package fr.beyondtime.view;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+
+public class MapView {
+    private GridPane gridPane;
+
+    public MapView(String imagePath) {
+        gridPane = new GridPane();
+        setBackground(imagePath);
+    }
+
+    private void setBackground(String imagePath) {
+        Image backgroundImage = new Image(imagePath);
+        BackgroundImage background = new BackgroundImage(
+            backgroundImage,
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundPosition.CENTER,
+            new BackgroundSize(100, 100, true, true, true, false)
+        );
+        gridPane.setBackground(new Background(background));
+    }
+
+    public GridPane getGridPane() {
+        return gridPane;
+    }
+}
+
+
+
+
+

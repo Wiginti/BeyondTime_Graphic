dba8eba8769a43ba77c0c039fd193c9522b76af8
Slybald <nico.moreau2003@gmail.com>
2025-02-21 11:54:45 +0100

    menuview dans main

--- src/fr/beyondtime/main/Main.java
+++ src/fr/beyondtime/main/Main.java
@@ -1,69 +1,15 @@
 package fr.beyondtime.main;
 
 import fr.beyondtime.view.MenuView;
-import fr.beyondtime.view.editor.EditorView;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class Main extends Application {
-
-	private void showMenuScene(Stage primaryStage) {
-		MenuView menuPrincipal = new MenuView(false);
-		Scene sceneMenu = new Scene(menuPrincipal);
-		sceneMenu.getStylesheets().add(
-				Objects.requireNonNull(getClass().getResource("/fr/beyondtime/resources/style.css")).toExternalForm()
-		);
-		menuPrincipal.getJouerButton().setOnAction(event -> showNiveauScene(primaryStage));
-		menuPrincipal.getScoresButton().setOnAction(event -> {
-			System.out.println("affichage score a faire plus tard");
-		});
-		menuPrincipal.getQuitterButton().setOnAction(event -> primaryStage.close());
-
-		primaryStage.setScene(sceneMenu);
-		primaryStage.setFullScreen(true);
-		primaryStage.show();
-	}
-
-	private void showNiveauScene(Stage primaryStage) {
-		MenuView choixNiveau = new MenuView(true);
-		Scene sceneNiveau = new Scene(choixNiveau);
-		sceneNiveau.getStylesheets().add(
-				Objects.requireNonNull(getClass().getResource("/fr/beyondtime/resources/style.css")).toExternalForm()
-		);
-
-		// Bouton pour retourner au menu principal
-		choixNiveau.getRetourBtn().setOnAction(event -> showMenuScene(primaryStage));
-
-		// Ajout d'un écouteur sur le bouton "Niveau Personnalisé"
-		choixNiveau.getPersonnaliseBtn().setOnAction(event -> showEditorScene(primaryStage));
-
-		primaryStage.setScene(sceneNiveau);
-		primaryStage.setFullScreen(true);
-		primaryStage.show();
-	}
-
-	// Nouvelle méthode permettant d'afficher l'interface de l'éditeur (EditorView)
-	private void showEditorScene(Stage primaryStage) {
-		EditorView editorView = new EditorView();
-		Scene sceneEditor = new Scene(editorView);
-		sceneEditor.getStylesheets().add(
-				Objects.requireNonNull(getClass().getResource("/fr/beyondtime/resources/style.css")).toExternalForm()
-		);
-
-		// (Optionnel) Vous pouvez prévoir un bouton ou un autre mécanisme dans EditorView pour revenir en arrière.
-
-		primaryStage.setScene(sceneEditor);
-		primaryStage.setFullScreen(true);
-		primaryStage.show();
-	}
 
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Menu de Jeu");
-		showMenuScene(primaryStage);
+		MenuView.showMenuScene(primaryStage);
 	}
 
 	public static void main(String[] args) {
--- src/fr/beyondtime/view/MenuView.java
+++ src/fr/beyondtime/view/MenuView.java
@@ -1,8 +1,13 @@
 package fr.beyondtime.view;
 
+import fr.beyondtime.view.editor.EditorView;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class MenuView extends VBox {
 
@@ -60,5 +65,54 @@ public class MenuView extends VBox {
 
     public Button getRetourBtn() {
         return retourBtn;
+    }
+
+    // Affiche le menu principal
+    public static void showMenuScene(Stage stage) {
+        MenuView menuPrincipal = new MenuView(false);
+        Scene sceneMenu = new Scene(menuPrincipal);
+        sceneMenu.getStylesheets().add(
+                Objects.requireNonNull(MenuView.class.getResource("/fr/beyondtime/resources/style.css")).toExternalForm()
+        );
+
+        menuPrincipal.getJouerButton().setOnAction(event -> showNiveauScene(stage));
+        menuPrincipal.getScoresButton().setOnAction(event -> {
+            System.out.println("affichage score à faire plus tard");
+        });
+        menuPrincipal.getQuitterButton().setOnAction(event -> stage.close());
+
+        stage.setScene(sceneMenu);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
+    // Affiche le choix du niveau
+    public static void showNiveauScene(Stage stage) {
+        MenuView choixNiveau = new MenuView(true);
+        Scene sceneNiveau = new Scene(choixNiveau);
+        sceneNiveau.getStylesheets().add(
+                Objects.requireNonNull(MenuView.class.getResource("/fr/beyondtime/resources/style.css")).toExternalForm()
+        );
+
+        choixNiveau.getRetourBtn().setOnAction(event -> showMenuScene(stage));
+        choixNiveau.getPersonnaliseBtn().setOnAction(event -> showEditorScene(stage));
+        // Vous pouvez également définir une action pour le bouton classique
+
+        stage.setScene(sceneNiveau);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
+    // Affiche l'interface de l'éditeur
+    public static void showEditorScene(Stage stage) {
+        EditorView editorView = new EditorView();
+        Scene sceneEditor = new Scene(editorView);
+        sceneEditor.getStylesheets().add(
+                Objects.requireNonNull(MenuView.class.getResource("/fr/beyondtime/resources/style.css")).toExternalForm()
+        );
+
+        stage.setScene(sceneEditor);
+        stage.setFullScreen(true);
+        stage.show();
     }
 }
